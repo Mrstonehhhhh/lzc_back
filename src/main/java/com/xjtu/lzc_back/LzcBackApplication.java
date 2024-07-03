5f144709d18dbf21d0f767dcbe78cdc640600b86
LZC <443019785@qq.com>
2024-07-03 17:09:12 +0800

    11

---
+++ src/main/java/com/xjtu/lzc_back/LzcBackApplication.java
@@ -0,0 +1,13 @@
+package com.xjtu.lzc_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzcBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzcBackApplication.class, args);
+    }
+
+}
